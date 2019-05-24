a820bbd3e36a1c9a82bb6400138d795247c7a9dc
Deslaygo <xavier11.jd@gmail.com>
2019-05-24 13:21:43 -0500

    Se inicio el repositorio

---
+++ Gato.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_List
+{
+    class Gato
+    {
+        //Propiedades
+        private int id;
+        private string nombre;
+        private string color;
+
+        public int Id { get => id; set => id = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Color { get => color; set => color = value; }
+
+        //Constructor
+        public Gato()
+        {
+            Id = 0;
+            Nombre = "";
+            Color = "";
+        }
+    }
+}
