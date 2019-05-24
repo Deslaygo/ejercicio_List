e11cf076cb03004e5fcf278cc52c92be57924e9d
Deslaygo <xavier11.jd@gmail.com>
2019-05-24 13:37:45 -0500

    Se agregaron propiedades a la clase perro

--- Perro.cs
+++ Perro.cs
@@ -8,5 +8,11 @@ namespace ejercicio_List
 {
     public class Perro
     {
+        //Propiedades
+        private string nombre;
+        private int edad;
+        //Encapsulación
+        public string Nombre { get => nombre; set => nombre = value; }
+        public int Edad { get => edad; set => edad = value; }
     }
 }
